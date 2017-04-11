5603f80a1793d86b5601f1eb1636eedb0a73aa2c
Callan Morrison <callan.l.morrison@gmail.com>
2017-04-10 19:54:23 -0500

    dino printing

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,24 @@
-var myString = `Callan wins all cage matches ${2 + 5}`;
+var dinoArray = [];
+
+$.ajax('./db/dinosaurs.json').done(function(data) {
+	dinoArray = data.dinosaurs;
+	makeDom(dinoArray);
+}).fail(function(error){
+	console.log("You've made a huge mistake", error);
+});
+
+function makeDom(myArrayToPrint) {
+	var myDomString = "";
+	for (var i = 0; i < myArrayToPrint.length; i++) {
+		myDomString += `<div class="dinoCard">`;
+		myDomString += `<header><h1>${myArrayToPrint[i].type}</h1></header>`;
+		myDomString += `<section>`;
+		myDomString += `<img src="${myArrayToPrint[i].img}">`;
+		myDomString += `<p class="bio">${myArrayToPrint[i].bio}</p>`;
+		myDomString += `</section>`;
+		myDomString += `<footer>${myArrayToPrint[i].info}</footer>`;
+		myDomString += `</div>`;
+	}
+	$("#dinosaurs").append(myDomString);
+}
+
